6f0586b0765db491c4b7716619446e4dbeb62335
Daniel Bravo <daniel.brv@hotmail.cl>
2021-08-13 11:49:47 -0400

    Se agrega modificación de HTML  desde javascript, clase escribiendo HTML desde JavasScript

--- descuentos.js
+++ descuentos.js
@@ -5,5 +5,15 @@ function calcularPrecioConDescuento(precio,descuento){
     return precioConDescuento;
 }
 
+function onClickButtonPriceDiscount(){
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+    const InputDiscount = document.getElementById("InputDiscount");
+    const discountValue = InputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue,discountValue);
+    const resultP = document.getElementById("ResultP");
+    ResultP.innerText = "El precio con descuento son: $" + precioConDescuento;    
+}
 
 
