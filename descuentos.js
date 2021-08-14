ded6261c9543129c3ab5245bb63f02926db7d571
Daniel Bravo <daniel.brv@hotmail.cl>
2021-08-14 12:37:28 -0400

    Se modifican valores para que el descuento sea el original del taller2 y no el ejercico opcion1

--- descuentos.js
+++ descuentos.js
@@ -13,26 +13,10 @@ function calcularPrecioConDescuento(precio,descuento){
 function onClickButtonPriceDiscount(){
     const inputPrice = document.getElementById("InputPrice");
     const priceValue = inputPrice.value;
-    const inputCoupon = document.getElementById("InputCoupon");
-    const couponValue = inputCoupon.value;
+    const InputDiscount = document.getElementById("InputDiscount");
+    const discountValue = InputDiscount.value;
 
-    let descuento;
-
-  switch(couponValue) {
-    case "JuanDC_es_Batman":
-      descuento = 15;
-    break;
-    case "pero_no_le_digas_a_nadie":
-      descuento = 30;
-    break;
-    case "es_un_secreto":
-      descuento = 25;
-    break;
-    default:
-      descuento = 0;
-  }
-
-    const precioConDescuento = calcularPrecioConDescuento(priceValue,descuento);
+    const precioConDescuento = calcularPrecioConDescuento(priceValue,discountValue);
     const resultP = document.getElementById("ResultP");
     ResultP.innerText = "El precio con descuento son: $" + precioConDescuento;    
 }
