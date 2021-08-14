4514b90d236dd053b8a9283b3bc74849cc51a52c
Daniel Bravo <daniel.brv@hotmail.cl>
2021-08-14 12:12:34 -0400

    Se agrega la solución 1 del ejercio del taller 2, ademaas se modifican los estilos

--- descuentos.js
+++ descuentos.js
@@ -1,3 +1,8 @@
+const coupons = [
+    "JuanDC_es_Batman",
+    "pero_no_le_digas_a_nadie",
+    "es_un_secreto",
+];
 
 function calcularPrecioConDescuento(precio,descuento){
     const porcentajePrecioConDescuento = 100 - descuento;
@@ -8,10 +13,26 @@ function calcularPrecioConDescuento(precio,descuento){
 function onClickButtonPriceDiscount(){
     const inputPrice = document.getElementById("InputPrice");
     const priceValue = inputPrice.value;
-    const InputDiscount = document.getElementById("InputDiscount");
-    const discountValue = InputDiscount.value;
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
 
-    const precioConDescuento = calcularPrecioConDescuento(priceValue,discountValue);
+    let descuento;
+
+  switch(couponValue) {
+    case "JuanDC_es_Batman":
+      descuento = 15;
+    break;
+    case "pero_no_le_digas_a_nadie":
+      descuento = 30;
+    break;
+    case "es_un_secreto":
+      descuento = 25;
+    break;
+    default:
+      descuento = 0;
+  }
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue,descuento);
     const resultP = document.getElementById("ResultP");
     ResultP.innerText = "El precio con descuento son: $" + precioConDescuento;    
 }
